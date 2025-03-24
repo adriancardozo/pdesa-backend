d8650a42f3e791f17f534068018e2731b7cd1fdc
Adrián Cardozo <cardozoadrian997@gmail.com>
2025-03-24 16:31:45 -0300

    Add eslint configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,11 @@ export default tseslint.config(
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         { vars: 'all', args: 'none', ignoreRestSiblings: false },
