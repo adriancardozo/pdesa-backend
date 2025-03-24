6a569bd8d07ba299d06273dd3e281024b687984e
Adrián Cardozo <cardozoadrian997@gmail.com>
2025-03-24 16:27:08 -0300

    Add eslint, prettier and jest configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,9 +27,23 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { vars: 'all', args: 'none', ignoreRestSiblings: false },
+      ],
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'auto',
+          singleQuote: true,
+          trailingComma: 'all',
+          printWidth: 108,
+        },
+      ],
     },
   },
 );
